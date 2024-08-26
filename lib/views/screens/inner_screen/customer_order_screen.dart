a4e2cff1df575eb5e9ec6d9b24e2311bffcf314a
lazuardi imani <lazuardi.crs@gmail.com>
2024-08-26 10:29:55 +0700

    lesson-248

--- lib/views/screens/inner_screen/customer_order_screen.dart
+++ lib/views/screens/inner_screen/customer_order_screen.dart
@@ -1,16 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:intl/intl.dart';
 
 class CustomerOrderScreen extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  double rating = 0;
+  final TextEditingController _reviewController = TextEditingController();
 
   String formatedDate(date){
     final outputDateFormate = DateFormat("dd/MM/yyyy");
-    final outputDate = outputDateFormate.format(date);
+    final outputDate = outputDateFormate.format(date.toDate());
 
     return outputDate;
+  }
+
+  Future<bool> hasUserReviewedProduct(String productId) async {
+    final user = FirebaseAuth.instance.currentUser;
+    final QuerySnapshot = await FirebaseFirestore.instance
+    .collection('productReviews')
+    .where('buyerId', isEqualTo: user!.uid)
+    .where('productId',isEqualTo: productId)
+    .get();
+
+    return QuerySnapshot.docs.isNotEmpty;
   }
 
   @override
@@ -85,6 +99,65 @@ class CustomerOrderScreen extends StatelessWidget {
                                 padding: const EdgeInsets.all(8.0),
                                 child: Text('order date : '+formatedDate(data['orderDate'])),
                               ),
+                              data['accepted'] == true ?
+                              ElevatedButton(onPressed: () {
+                                showDialog(context: context, builder: (context) {
+                                  return AlertDialog(
+                                    title: Text('Leave a review'),
+                                    content: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        TextFormField(
+                                          controller: _reviewController,
+                                          decoration: InputDecoration(
+                                            labelText: 'Your Review'
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: RatingBar.builder(
+                                            initialRating: rating,
+                                            itemCount: 5,
+                                            minRating: 1,
+                                            maxRating: 5,
+                                            allowHalfRating: true,
+                                            itemSize: 15,
+                                            unratedColor: Colors.grey,
+                                            itemPadding: EdgeInsets.symmetric(horizontal: 4),
+                                            itemBuilder: (context, _) {
+                                            return Icon(Icons.star,
+                                            color: Colors.amber);
+                                          }, onRatingUpdate: (value) {
+                                            rating = value;
+                                          },),
+                                        )
+                                      ],
+                                    ),
+                                    actions: [
+                                      TextButton(onPressed: () async {
+                                        final review = _reviewController.text;
+
+                                        await FirebaseFirestore.instance.collection('productReviews')
+                                        .doc(data['orderId'])
+                                        .set(
+                                          {
+                                            'productId':data['productId'],
+                                            'fullName': data['fullName'],
+                                            'buyerId':data['buyerId'],
+                                            'rating':rating,
+                                            'review':review,
+                                            'email':data['email']
+                                          }
+                                        ).whenComplete(() {
+                                          Navigator.pop(context);
+                                          _reviewController.clear();
+                                        },);
+                                      }, child: Text('Submit'))
+                                    ],
+                                  );
+                                },);
+                              }, child: Text('Review'))
+                              : Text('')
                             ],
                           ),
                         ),
